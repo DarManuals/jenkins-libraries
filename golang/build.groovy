e4cda798936f8c0a10b11ab3222f0d63e4c0e4b5
Bogdan <19273017+BoRuDar@users.noreply.github.com>
2019-10-12 23:09:31 +0200

    golang build job.

---
+++ golang/build.groovy
@@ -0,0 +1,17 @@
+void call() {
+    stage("print") {
+        println "go: build()"
+    }
+
+    stage('git') { // for display purposes
+        git 'https://github.com/DarManuals/clean-arch.git'
+    }
+
+    stage("build") {
+        sh("go build .")
+    }
+
+    stage("test") {
+        sh("go test -v ./...")
+    }
+}
